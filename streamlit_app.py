fea698ec889dfdc328249da1373f05590a97003e
wwwmmmike <163374155+wwwmmmike@users.noreply.github.com>
2024-03-15 14:27:10 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
