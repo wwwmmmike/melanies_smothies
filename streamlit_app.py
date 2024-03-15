21dd67d571fe888065e925b3c90ce6c5361cc1e7
wwwmmmike <163374155+wwwmmmike@users.noreply.github.com>
2024-03-15 15:15:58 +1100

    Update streamlit_app.py
    my_dataframe search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,16 @@ st.title("🥤 My Parents New Healthy Dinner :cup_with_straw:")
 st.write(
     """Breakfast Menu"""
 )
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+                                                                      
 ingredients_list = st.multiselect(
     'Choose uop to 5 ingredients:'
     , my_dataframe
@@ -36,8 +37,8 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
 
-    st.write(my_insert_stmt)
-    st.stop
+    --st.write(my_insert_stmt)
+    --st.stop
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
